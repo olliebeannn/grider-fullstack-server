02ea42f66b15a4700da7529594d88cc81e38930c
Ollie Khakwani <okhakwani@gmail.com>
2019-03-26 20:15:15 -0400

    added client frontend, dual server npm dev command, refactored passport.js to use async/await

--- services/passport.js
+++ services/passport.js
@@ -21,20 +21,16 @@ passport.use(
 			callbackURL: '/auth/google/callback',
 			proxy: true
 		},
-		(accessToken, refreshToken, profile, done) => {
-			// console.log('access token:', accessToken);
-			// console.log('refresh token:', refreshToken);
-			// console.log('profile:', profile);
-			User.findOne({ googleId: profile.id }).then(existingUser => {
-				if (existingUser) {
-					console.log(existingUser.googleId);
-					done(null, existingUser);
-				} else {
-					new User({ googleId: profile.id })
-						.save()
-						.then(user => done(null, user));
-				}
-			});
+		async (accessToken, refreshToken, profile, done) => {
+			const existingUser = await User.findOne({ googleId: profile.id });
+
+			if (existingUser) {
+				console.log(existingUser.googleId);
+				return done(null, existingUser);
+			}
+
+			const user = await new User({ googleId: profile.id }).save();
+			done(null, user);
 		}
 	)
 );
