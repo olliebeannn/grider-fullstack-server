430cf961e20ed213ed54344a9c7f78651dd1a1cd
Ollie Khakwani <okhakwani@gmail.com>
2019-03-28 08:57:50 -0400

    Deleted React default files, added reducer and dummy react-router content

---
+++ client/src/components/App.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import { BrowserRouter, Route } from 'react-router-dom';
+
+const Header = () => <h2>Header!</h2>;
+const Dashboard = () => <h2>Dashboard!</h2>;
+const SurveyNew = () => <h2>SurveyNew!</h2>;
+const Landing = () => <h2>Landing!</h2>;
+
+class App extends Component {
+	render() {
+		return <div>Hi there!</div>;
+	}
+}
+
+export default App;
