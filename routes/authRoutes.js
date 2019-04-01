3ec4969359491a88c38f9ce7ba5b2234ad510024
Ollie Khakwani <okhakwani@gmail.com>
2019-03-31 22:17:19 -0400

    Added redirects to login and logout

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -8,7 +8,13 @@ module.exports = app => {
 		})
 	);
 
-	app.get('/auth/google/callback', passport.authenticate('google'));
+	app.get(
+		'/auth/google/callback',
+		passport.authenticate('google'),
+		(req, res) => {
+			res.redirect('/surveys');
+		}
+	);
 
 	// Route to test auth flow
 	app.get('/api/currentUser', (req, res) => {
@@ -18,6 +24,6 @@ module.exports = app => {
 	// Log out route
 	app.get('/api/logout', (req, res) => {
 		req.logout();
-		res.send(req.user);
+		res.redirect('/');
 	});
 };
