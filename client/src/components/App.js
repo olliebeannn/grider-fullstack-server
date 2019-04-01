96e6c9d95b793ec07ed8ec0cf81fdce050d2ebfe
Ollie Khakwani <okhakwani@gmail.com>
2019-03-31 22:09:24 -0400

    Work on frontend - created header component, connected redux and update header based on login state

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,15 +1,35 @@
 import React, { Component } from 'react';
 import { BrowserRouter, Route } from 'react-router-dom';
+import { connect } from 'react-redux';
+import * as actions from '../actions';
 
-const Header = () => <h2>Header!</h2>;
+import Header from './Header';
 const Dashboard = () => <h2>Dashboard!</h2>;
 const SurveyNew = () => <h2>SurveyNew!</h2>;
 const Landing = () => <h2>Landing!</h2>;
 
 class App extends Component {
+	componentDidMount() {
+		this.props.fetchUser();
+	}
+
 	render() {
-		return <div>Hi there!</div>;
+		return (
+			<div className="container">
+				<BrowserRouter>
+					<div>
+						<Header />
+						<Route path="/" exact component={Landing} />
+						<Route path="/surveys" exact component={Dashboard} />
+						<Route path="/surveys/new" component={SurveyNew} />
+					</div>
+				</BrowserRouter>
+			</div>
+		);
 	}
 }
 
-export default App;
+export default connect(
+	null,
+	actions
+)(App);
