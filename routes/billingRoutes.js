d96715ed3534d6cf1692105acf663a7408f9b9af
Ollie Khakwani <okhakwani@gmail.com>
2019-04-23 08:08:28 -0400

    update User model to include credits and billingRoutes to increment credits on payment

--- routes/billingRoutes.js
+++ routes/billingRoutes.js
@@ -1,8 +1,10 @@
 const keys = require('../config/keys');
 const stripe = require('stripe')(keys.stripeSecretKey);
 
+const requireLogin = require('../middlewares/requireLogin');
+
 module.exports = app => {
-	app.post('/api/stripe', async (req, res) => {
+	app.post('/api/stripe', requireLogin, async (req, res) => {
 		console.log(req.body);
 
 		const charge = await stripe.charges.create({
@@ -13,5 +15,10 @@ module.exports = app => {
 		});
 
 		console.log(charge);
+
+		req.user.credits += 5;
+		const user = await req.user.save();
+
+		res.send(user);
 	});
 };
