952b8ee97e7e59ca6c2558fe0a935dbfff04b654
Ollie Khakwani <okhakwani@gmail.com>
2019-05-07 08:32:07 -0400

    completed backend for POST /api/surveys, can send emails and link to confirmation page when giving feedback

--- client/src/index.js
+++ client/src/index.js
@@ -9,6 +9,10 @@ import 'materialize-css/dist/css/materialize.min.css';
 import App from './components/App';
 import reducers from './reducers';
 
+// Dev only - testing sendgrid
+import axios from 'axios';
+window.axios = axios;
+
 const store = createStore(reducers, {}, applyMiddleware(reduxThunk));
 
 ReactDOM.render(
