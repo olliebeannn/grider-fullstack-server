3223a6692981d6724706429c3ca975b302fc790c
Ollie Khakwani <okhakwani@gmail.com>
2019-05-08 22:17:37 -0400

    Added survey submission, hooked up to backend to actually send email with survey

--- client/src/components/surveys/SurveyNew.js
+++ client/src/components/surveys/SurveyNew.js
@@ -1,15 +1,31 @@
 import React, { Component } from 'react';
+import { reduxForm } from 'redux-form';
 
 import SurveyForm from './SurveyForm';
+import SurveyFormReview from './SurveyFormReview';
 
 class SurveyNew extends Component {
-  render() {
+  state = { showReview: false };
+
+  renderContent() {
+    if (this.state.showReview) {
+      return (
+        <SurveyFormReview
+          onCancel={() => this.setState({ showReview: false })}
+        />
+      );
+    }
+
     return (
-      <div>
-        <SurveyForm />
-      </div>
+      <SurveyForm onSurveySubmit={() => this.setState({ showReview: true })} />
     );
+  }
+
+  render() {
+    return <div>{this.renderContent()}</div>;
   }
 }
 
-export default SurveyNew;
+export default reduxForm({
+  form: 'surveyForm'
+})(SurveyNew);
